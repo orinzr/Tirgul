37c362c05589b8c2e13c098e8e05f77b30cc4d7f
Student <Student@10.0.10.122>
2016-03-01 16:30:42 +0200

    second commit

--- Tirgul1/src/T1.java
+++ Tirgul1/src/T1.java
@@ -4,6 +4,7 @@ public class T1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("osher hu ana leha?");
+		System.out.println("nu taane kvar ya zain");
 
 	}
 
